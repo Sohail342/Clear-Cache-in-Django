e4d21106a5f58e29c1e1c36ec1f5afb1be99dc9c
= <sohailahmed34280@gmail.com>
2024-08-02 06:29:21 -0700

    media files configration

--- Nasir_Shop/views.py
+++ Nasir_Shop/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
+from .models import Category
 
 # Create your views here.
 def index(request):
-    return render(request, "signup.html")
+    products = Product.objects.all()
+    return render(request, "index.html", {"products":products})
 
 
 def contact(request):
@@ -10,5 +13,20 @@ def contact(request):
 
 
 
-def products(request):
-    return render(request, 'products.html')
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products':products})
+
+
+def product(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'each_product.html', {'product':product})
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, "category.html", {'categories': categories})
+
+def catogory(request, name):
+    category = get_object_or_404(Category, name=name)
+    products = Product.objects.filter(category=category)
+    return render(request, "each_category.html", {'category': category, 'products': products})
